8a84592ed1c7e07e492972f1fe517783bb4a5838
Juan Pablo Rodriguez Morales <juan.morales@unibague.edu.co>
2019-03-08 17:46:35 -0500

    Created repo

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.hello;
+
+import javax.swing.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"HELLO WORLD");
+    }
+
+}
